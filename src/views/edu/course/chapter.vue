31283eea1d4ea093ca5ec7c52dd813988893bfc1
lv-debug <lvgr888@163.com>
2021-04-08 22:55:48 +0800

    删除小节

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -72,7 +72,25 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="上传视频">
-          <!-- TODO -->
+          <el-upload
+                :on-success="handleVodUploadSuccess"
+                :on-remove="handleVodRemove"
+                :before-remove="beforeVodRemove"
+                :on-exceed="handleUploadExceed"
+                :file-list="fileList"
+                :action="BASE_API+'/eduvod/video/uploadAlyiVideo'"
+                :limit="1"
+                class="upload-demo">
+            <el-button size="small" type="primary">上传视频</el-button>
+            <el-tooltip placement="right-end">
+                <div slot="content">最大支持1G，<br>
+                    支持3GP、ASF、AVI、DAT、DV、FLV、F4V、<br>
+                    GIF、M2T、M4V、MJ2、MJPEG、MKV、MOV、MP4、<br>
+                    MPE、MPG、MPEG、MTS、OGG、QT、RM、RMVB、<br>
+                    SWF、TS、VOB、WMV、WEBM 等视频格式上传</div>
+                <i class="el-icon-question"/>
+            </el-tooltip>
+          </el-upload>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -102,8 +120,12 @@ export default {
       video: {
         title: '',
         sort: 0,
-        free: '',
+        free: 0,
+        videoSourceId:'',
+        videoOriginalName:''
       },
+      fileList: [],//上传文件列表
+      BASE_API: process.env.BASE_API // 接口API地址
     }
   },
   
@@ -168,6 +190,26 @@ export default {
                   this.getChapterVideo()
                 })
             })
+    },
+    //上传视频成功调用
+    handleVodUploadSuccess(response,file,fileList) {
+      this.video.videoSourceId = response.data.videoId
+      this.video.videoOriginalName = file.name
+
+    },
+
+    //上传之前调用
+    handleUploadExceed() {
+      this.$message({type: 'warning',message: '重新上传，请先删除之前上传的视频!'})
+    },
+
+    //删除小节之前
+    beforeVodRemove(file,fileList) {
+      return this.$confirm(`确定删除${ file.name }吗？`)
+    },
+    //删除小节
+    handleVodRemove() {
+
     },
 
     //-------------------------------------章节--------------------------------
